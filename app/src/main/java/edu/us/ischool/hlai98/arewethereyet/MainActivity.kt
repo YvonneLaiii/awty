a4180dee9215819e2fe903daa87544cbeb62821f
YvonneLaiii <hlai98@uw.edu>
2019-05-21 17:11:07 -0700

    ok

--- app/src/main/java/edu/us/ischool/hlai98/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/hlai98/arewethereyet/MainActivity.kt
@@ -1,15 +1,19 @@
 package edu.us.ischool.hlai98.arewethereyet
 
+import android.Manifest.permission.SEND_SMS
 import android.app.AlarmManager
 import android.app.AlarmManager.RTC_WAKEUP
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
@@ -19,7 +23,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val PERMISSION = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,9 +32,9 @@ class MainActivity : AppCompatActivity() {
         val msg = findViewById<EditText>(R.id.message)
         val phone = findViewById<EditText>(R.id.phone)
         val minutes = findViewById<EditText>(R.id.minutes)
-        val alarmManager: AlarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(this@MainActivity, StartActivity::class.java)
-
+        //val alarmManager: AlarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        //Log.i(TAG, "1.")
+        checkPhonePermission()
         msg.addTextChangedListener(object :TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
@@ -48,24 +52,45 @@ class MainActivity : AppCompatActivity() {
         })
         btn.setOnClickListener{
             if (!(msg.text.isNotEmpty() && phone.text.isNotEmpty() && minutes.text.isNotEmpty())) {
-                Toast.makeText(this@MainActivity, "Please fill all the information!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Please fill all the information!", Toast.LENGTH_LONG).show()
             } else if (!(minutes.text.toString().toInt() > 0)) {
-                Toast.makeText(this@MainActivity, "Please enter valid minute value (integers that are greater than 0)", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Please enter valid minute value (integers that are greater than 0)", Toast.LENGTH_LONG).show()
             } else if (!(phone.text.toString().length == 10)) {
-                Toast.makeText(this@MainActivity, "Please enter valid phone number!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Please enter valid phone number!", Toast.LENGTH_LONG).show()
             } else {
                 if (btn.text == "Start") {
                     btn.text = "Stop"
-                    intent.putExtra("msg", msg.text.toString())
-                    intent.putExtra("phone", phone.text.toString())
-                    val pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, intent,PendingIntent.FLAG_CANCEL_CURRENT)
-                    alarmManager!!.setRepeating(RTC_WAKEUP, System.currentTimeMillis(),(minutes.text.toString().toLong() * 60 * 1000), pendingIntent)
+                    val time = Integer.parseInt(minutes.text.toString())
+                    val message = msg.text.toString()
+                    val number = phone.text.toString()
+                    getStart(time, message, number)
+                    //intent.putExtra("msg", msg.text.toString())
+                    //intent.putExtra("phone", phone.text.toString())
+                    //val pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, intent,PendingIntent.FLAG_CANCEL_CURRENT)
+                    //alarmManager!!.setRepeating(RTC_WAKEUP, System.currentTimeMillis(),(minutes.text.toString().toLong() * 60 * 1000), pendingIntent)
                 } else {
                     btn.text = "Start"
-                    alarmManager!!.cancel(PendingIntent.getBroadcast(applicationContext, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT))
+                    stopEvent()
+                    //alarmManager!!.cancel(PendingIntent.getBroadcast(applicationContext, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT))
                 }
             }
         }
     }
-
+    private fun getStart(time: Int, msg: String, phone:String) {
+        val intent = Intent(this@MainActivity, StartActivity::class.java)
+        intent.putExtra("msg", msg)
+        intent.putExtra("phone", phone)
+        intent.putExtra("time", time)
+        startService(intent)
+        //val pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, intent,PendingIntent.FLAG_CANCEL_CURRENT)
+        //alarmManager!!.setRepeating(RTC_WAKEUP, System.currentTimeMillis(),(minutes.text.toString().toLong() * 60 * 1000), pendingIntent)
+    }
+    private fun stopEvent() {
+        stopService(Intent(this@MainActivity,StartActivity::class.java))
+    }
+    private fun checkPhonePermission() {
+        if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, SEND_SMS)) {
+            ActivityCompat.requestPermissions(this, arrayOf(SEND_SMS), PERMISSION)
+        }
+    }
 }
--- app/src/main/java/edu/us/ischool/hlai98/arewethereyet/StartActivity.kt
+++ app/src/main/java/edu/us/ischool/hlai98/arewethereyet/StartActivity.kt
@@ -1,15 +1,57 @@
 package edu.us.ischool.hlai98.arewethereyet
 
+import android.app.IntentService
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.telephony.SmsManager
+import android.util.Log
 import android.widget.Toast
 
-class StartActivity : BroadcastReceiver() {
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        val msg = p1!!.getStringExtra("msg")
-        val phone = p1!!.getStringExtra("phone")
-        val message = "(" + phone.substring(0..2) + ")" + phone.substring(3..5) + "-" + phone.substring(6)
-        Toast.makeText(p0, "$message : $msg", Toast.LENGTH_LONG).show()
+class StartActivity : IntentService("StartActivity") {
+
+   // override fun onReceive(p0: Context?, p1: Intent?) {
+      //  Log.i(TAG, "start sub")
+      //  val msg = p1!!.getStringExtra("msg")
+      //  val phone = p1!!.getStringExtra("phone")
+      //  val message = "(" + phone.substring(0..2) + ")" + phone.substring(3..5) + "-" + phone.substring(6)
+       // Toast.makeText(p0, "$message : $msg", Toast.LENGTH_LONG).show()
+   // }
+    private val myHandle = Handler()
+    private var again = true
+
+    override fun onDestroy() {
+        again = false
+        super.onDestroy()
     }
+    override fun onHandleIntent(p0: Intent?) {
+        var time : Int
+        var msg: String
+        var phone : String
+        p0?.extras.apply{
+            time = this!!.getInt("time")
+            msg = this!!.getString("msg")
+            phone = this!!.getString("phone")
+            phone = phone.substring(0..2) + ")" + phone.substring(3..5) + "-" + phone.substring(6)
+        }
+        while (again) {
+            myHandle.post {
+                try {
+                    Toast.makeText(this@StartActivity, "Sending", Toast.LENGTH_LONG).show()
+                    val managerSMS = SmsManager.getDefault() as SmsManager
+                    managerSMS.sendTextMessage(phone,null,msg,null,null)
+                    Toast.makeText(this@StartActivity, "SMS sent to $phone ($msg)", Toast.LENGTH_LONG).show()
+                } catch (e:Exception) {
+                    Toast.makeText(this@StartActivity, "SMS failed to send.", Toast.LENGTH_LONG).show()
+                }
+            }
+            try {
+                Thread.sleep((time*60*1000).toLong())
+            } catch (e: InterruptedException) {
+                Thread.currentThread().interrupt()
+            }
+        }
+    }
+
 }
